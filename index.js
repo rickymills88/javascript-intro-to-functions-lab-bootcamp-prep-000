b2acb2ddfc343f4fdcf2dad3fd8996a067e1bd4a
ricky_a_mills <ricky_a_mills@yahoo.co.uk>
2018-09-18 09:26:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase // true
+    if (string === uppercase) {
+      return 
+    } else if (string === 'HELLO') {
+      return string 
+    } else {
+      return string
+    }
+}
+
