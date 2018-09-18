d038f6aa9e6bfdf535ff3a944c9981f4ade67111
ricky_a_mills <ricky_a_mills@yahoo.co.uk>
2018-09-18 09:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ function sayHiToGrandma(string) {
 }
 
 
-it worked.. thank you.. my new code that passed!!
-function sayHiToGrandma(string) {
- var lowercase = "hello"
- var uppercase = "HELLO"
- if (string.toLowerCase() === string)
- return("I can't hear you!");
- else if (string.toUpperCase() === string) 
- return("YES INDEED!")
-  }
+// it worked.. thank you.. my new code that passed!!
+// function sayHiToGrandma(string) {
+// var lowercase = "hello"
+// var uppercase = "HELLO"
+// if (string.toLowerCase() === string)
+// return("I can't hear you!");
+// else if (string.toUpperCase() === string) 
+// return("YES INDEED!")
+//   }
 
