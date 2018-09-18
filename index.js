b6474e8d453993c1aeaadd5ec483177dffe44b20
ricky_a_mills <ricky_a_mills@yahoo.co.uk>
2018-09-18 09:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,35 +13,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase() === uppercase 
   
-    var lowercase = 'hello!'
-    lowercase.toLowerCase() === lowercase // true
-     
-    var mixedCase = 'I love you, Grandma.'
-    mixedCase.toLowerCase() === mixedCase // false
-    mixedCase.toUpperCase() === mixedCase 
-
-    if (string === uppercase) {
-      return ("YES INDEED!")
-    } else if (string === lowercase) {
-      return ("I can't hear you!")
-    } else if (string === mixedCase) {
-      return ("I love you, too.")
-    }
-}
 
 
-// it worked.. thank you.. my new code that passed!!
-// function sayHiToGrandma(string) {
-// var lowercase = "hello"
-// var uppercase = "HELLO"
-// if (string.toLowerCase() === string)
-// return("I can't hear you!");
-// else if (string.toUpperCase() === string) 
-// return("YES INDEED!")
-//   }
+
+it worked.. thank you.. my new code that passed!!
+function sayHiToGrandma(string) {
+var lowercase = "hello"
+var uppercase = "HELLO"
+if (string.toLowerCase() === string)
+return("I can't hear you!");
+else if (string.toUpperCase() === string) 
+return("YES INDEED!")
+  }
 
